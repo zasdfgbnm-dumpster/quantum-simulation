ca4387ef9522e875c07838155594efa2913a5f70
Gao Xiang <qasdfgtyuiop@gmail.com>
2013-07-09 03:58:11 +0800

    add U() test

---
+++ tests/test_U.cpp
@@ -0,0 +1,27 @@
+#include "../spin.hpp"
+#include <unsupported/Eigen/MatrixFunctions>
+#include <iostream>
+#include <ctime>
+int main() {
+	mt19937 engine(static_cast<unsigned long>(time(nullptr)));
+	poisson_distribution<int> dim_dist(10);
+	uniform_real_distribution<double> t_dist(0,1);
+	while(true) {
+		int dim = dim_dist(engine);
+		double t = t_dist(engine);
+		cout << "dimension: " << dim << endl;
+		MatrixXcd mat = MatrixXcd::Random(dim,dim);
+		/* calculate by Operator::U */
+		Operator op(0,mat);
+		auto U = op.U()(t);
+		/* calculate by matrix exponential */
+		MatrixXcd mat_exp = (-1_i*mat*t/hbar).exp();
+		/* calculate error */
+		MatrixXcd diff = U.matrix()-mat_exp;
+		double diff_sum = 0;
+		for(int i=0;i<dim;i++)
+			for(int j=0;j<dim;j++)
+				diff_sum += abs(diff(i,j));
+		cout << "error is:" << diff_sum << endl << endl;
+	}
+}
